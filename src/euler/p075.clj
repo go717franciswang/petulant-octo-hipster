e75f5b0ca987998257a5612fef05bf29f457a306
francis <francis@UbuntuServer>
2013-08-13 22:04:42 -0700

    Solve p075

--- src/euler/p075.clj
+++ src/euler/p075.clj
@@ -3,28 +3,18 @@
 
 ; Euclid's formula 
 ; a = k(mm - nn), b = k2mn, c = k(mm + nn) => L = 2kmm + 2kmn => L = 2km(m + n)
-; L with only one possible triple => L/2 is a product of 2 uniq numbers (either a prime or 1)
-; while m > n
 
 (def size 1500000)
-;(def size 150)
 
 (def m-cap (int (Math/pow (/ size 2) 0.5)))
 
 (def primes (euler.helper/primes m-cap))
 
 (count
-  (filter (comp (partial = 1) count second)
-    (reduce
-      (fn [m [l s]]
-        (let [v (conj (get m l #{}) s)]
-          (assoc m l v)))
-      {}
-      (for [[n m] (euler.helper/farey m-cap)
+  (filter (comp (partial = 1) second)
+    (frequencies
+      (for [[n m] (euler.helper/farey (* 2 m-cap))
             :let [l (* 2 m (+ m n))]
-            :when (and (odd? (- m n)) (> n 0))
+            :when (and (odd? (- m n)) (> n 0) (> m n))
             k (range 1 (inc (int (/ size l))))]
-        (let [a (* k (- (* m m) (* n n)))
-              b (* k m n)
-              c (* k (+ (* m m) (* n n)))]
-          [l #{a b c}])))))
+          (* l k)))))
