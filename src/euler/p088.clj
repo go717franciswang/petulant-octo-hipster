b456fe111b7e76fcbea974e859f600599e8ccb67
francis <go717franciswang@gmail.com>
2013-08-15 19:38:03 -0700

    Start p088

---
+++ src/euler/p088.clj
@@ -0,0 +1,6 @@
+(ns euler.p088)
+
+; paper on this subject "When the sum equals the product"
+; http://www-users.mat.umk.pl/~anow/ps-dvi/si-krl-a.pdf
+; Thm 4: x1 * .. * xn-1 <= n - 1
+; Thm 6: x1 + .. + xn <= 2n to bound search space
