7552bb36cc9888219cba57248126c4722eb3487f
francis <francis@UbuntuServer>
2013-08-15 22:27:14 -0700

    Brute force p088

--- src/euler/p088.clj
+++ src/euler/p088.clj
@@ -4,3 +4,33 @@
 ; http://www-users.mat.umk.pl/~anow/ps-dvi/si-krl-a.pdf
 ; Thm 4: x1 * .. * xn-1 <= n - 1
 ; Thm 6: x1 + .. + xn <= 2n to bound search space
+
+(defn min-product-sum [size]
+  (let [suffix (loop [candidates (map #(vector [%] %) (range 1 (inc size)))]
+                 (if (= (dec size) (count (first (first candidates))))
+                   candidates
+                   (recur 
+                     (for [[values product] candidates
+                           :let [max-val (last values)]
+                           next-val (range 1 (inc (min (quot size product) max-val)))]
+                       [(conj values next-val) (* product next-val)]))))]
+    (reduce min
+      (filter identity
+        (map
+          (fn [[series product]]
+            (let [sum (reduce + series)
+                  error (if (= 1 product) 1 (mod sum (dec product)))]
+              (when (zero? error)
+                (+ sum (quot sum (dec product))))))
+          suffix)))))
+
+(reduce +
+  (set 
+    (map 
+      (fn [s]
+        (let [a (min-product-sum s)]
+          (println [s a])))
+      (range 2 12001))))
+
+
+      
