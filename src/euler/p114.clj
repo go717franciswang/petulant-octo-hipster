55f2b9eee1f5bae6098e96b8ab7a51a8b51f5304
francis <francis@UbuntuServer>
2013-08-20 23:52:45 -0700

    Solve p114

--- src/euler/p114.clj
+++ src/euler/p114.clj
@@ -4,38 +4,28 @@
 
 (def total-length 50)
 
-(defn blocks [max-size]
-  (cons [:B 1] (map #(vector :R %) (range min-block-size (inc max-size)))))
+(def ways-red-start 
+  (memoize
+    (fn [length]
+      (if (< length min-block-size)
+        0
+        (reduce +
+          (for [red-length (range min-block-size (inc length))
+                black-length (range (inc (- length red-length)))
+                :let [left (- length red-length black-length)]]
+            (if (zero? left) 
+              1 
+              (if (> black-length 0) 
+                (ways-red-start left)
+                0))))))))
 
-(def result-init (map 
-                   (fn [[_ block-size :as block]]
-                     [(- total-length block-size) [block]])
-                   (blocks (dec total-length))))
+(defn ways-any-start [length]
+  (inc
+    (reduce +
+      (map
+        (fn [black-length]
+          (let [left (- length black-length)]
+            (ways-red-start left)))
+        (range (inc length))))))
 
-(inc (count
-  (loop [regen result-init
-         results []]
-    (println "regens count: " (count regen))
-    (if (empty? regen)
-      results
-      (let [regen-and-results (for [[left block-seq :as result] regen
-                                    :let [[last-block-color last-block-size] (last block-seq)]
-                                    [color size :as next-block] (blocks left)
-                                    :while (or (= last-block-color :B) (= color :B))
-                                    :let [new-left (- left size)]
-                                    :when (>= new-left 0)]
-                                (if (= last-block-color color :B)
-                                  (let [updated-block [:B (+ last-block-size size)]
-                                        last-idx (dec (count block-seq))
-                                        new-block-seq (assoc block-seq last-idx updated-block)]
-                                    [new-left new-block-seq])
-                                  [new-left (conj block-seq next-block)]))
-            [regen more-results] (reduce
-                                   (fn [[regen more-results] [left _ :as result]]
-                                     (if (zero? left)
-                                       [regen (conj more-results result)]
-                                       [(conj regen result) more-results])) 
-                                   [[] []]
-                                   regen-and-results)]
-        (recur regen (concat results more-results)))))))
-                                  
+(ways-any-start 50)
