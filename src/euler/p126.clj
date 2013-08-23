5716938fd7ff82d5ef717d8560b6fbe50a6bffe5
francis <francis@UbuntuServer>
2013-08-23 02:16:21 -0700

    Inefficiently solve p126

---
+++ src/euler/p126.clj
@@ -0,0 +1,26 @@
+(ns euler.p126)
+
+; get the following equation from observation
+; n = 2(ab + bc + ac) + 4(l - 1)(a + b + c + l - 2)
+; find the first n such that there exist 1000 integer solutions for [#{a b c} l]
+
+; the brute force approach is generating a lot of solutions for a lot of cuboids
+
+(def max-cuboid-size 10000)
+
+(def n-cap 19000)
+
+(defn cubes-required [a b c l]
+  (+ (* 2 (+ (* a b) (* b c) (* a c))) (* 4 (dec l) (+ a b c l -2))))
+
+(sort-by first
+  (filter #(= (second %) 1000)
+    (frequencies
+      (for [a (range 1 (inc max-cuboid-size))
+            b (range 1 (inc a))
+            c (range 1 (inc b))
+            l (rest (range))
+            :let [cubes (cubes-required a b c l)]
+            :while (<= cubes n-cap)]
+        (cubes-required a b c l)))))
+
