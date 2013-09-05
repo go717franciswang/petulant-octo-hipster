3af82732864cc172f791af17b7f8bf788a30aae3
francis <go717franciswang@gmail.com>
2013-09-05 14:24:39 -0700

    Solve p154

--- src/euler/p154.clj
+++ src/euler/p154.clj
@@ -2,43 +2,45 @@
 
 ; http://en.wikipedia.org/wiki/Trinomial_expansion
 ; coefficient = n!/(i!j!k!)
-; let i <= j <= k for now, count of total arrangement = * 6
+; let i+j+k = 2E6 and i <= j <= k for now, count of total arrangement = * 6
 
-(def n!-trailing-0s 
-  (memoize
-    (fn [n]
-      (loop [d 5
-             r 0]
-        (let [q (quot n d)]
-          (if (> q 0)
-            (recur (* d 5) (int (+ r q)))
-            r))))))
+(defn n!-factor-k [n k]
+  (loop [d k
+         r 0]
+    (let [q (quot n d)]
+      (if (> q 0)
+        (recur (* d k) (int (+ r q)))
+        r))))
 
-(def div 2E5)
+(def div (int 2E5))
 
-(def numerator-0s (n!-trailing-0s div))
-
-(def min-k
-  (loop [k (dec div)]
-    (if (> (- numerator-0s 12) (n!-trailing-0s (* 3 k)))
-      (inc k)
-      (recur (dec k)))))
-
-(def target-denom-0s (- numerator-0s 12))
+(def n!-5s (vec (map #(n!-factor-k % 5) (range 0 (inc div)))))
+(def n!-2s (vec (map #(n!-factor-k % 2) (range 0 (inc div)))))
 
 (def m {3 1 2 3 1 6})
 
-(- (* (+ div 2) (inc div) 0.5)
-  (reduce +
-    (for [k (range div (dec min-k) -1)
-          :let [denom-0s (n!-trailing-0s k)
-                max-j (min (- div k) k)
-                _ (println k)]
-          j (range max-j 0 -1)
-          :while (<= target-denom-0s (+ denom-0s (n!-trailing-0s (* 2 j))))
-          :let [denom-0s (+ denom-0s (n!-trailing-0s j))
-                max-i (min (- div k j) j)]
-          i (range max-i -1 -1)
-          :while (<= target-denom-0s (+ denom-0s (n!-trailing-0s i)))]
-      (m (count (set [k j i]))))))
+(def target-denom-5s (- (n!-5s div) 12))
+(def target-denom-2s (- (n!-2s div) 12))
+
+(defn valid? [n5 n2]
+  (and (<= n5 target-denom-5s)
+       (<= n2 target-denom-2s)))
+(println target-denom-2s target-denom-5s)
+
+(reduce +
+  (pmap
+    (fn [i]
+      (let [max-j (quot (- div i) 2)
+            n5 (n!-5s i)
+            n2 (n!-2s i)
+            i=j (if (valid? (+ n5 n5 (n!-5s (- div i i))) (+ n2 n2 (n!-2s (- div i i)))) 3 0)
+            j=k (if (and (zero? (mod (- div i) 2))
+                        (valid? (+ n5 (* 2 (n!-5s max-j))) (+ n2 (* 2 (n!-2s max-j))))) 3 0)]
+        (apply +
+          i=j j=k
+          (for [j (range (inc i) (if (zero? (mod (- div i) 2)) max-j (inc max-j)))
+                :let [k (- div i j)]
+                :when (valid? (+ n5 (n!-5s j) (n!-5s k)) (+ n2 (n!-2s j) (n!-2s k)))]
+            6))))
+    (range (inc (quot div 3)))))
 
