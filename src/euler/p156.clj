090806e14558a399e7305735cd45c8a9aef75704
francis <go717franciswang@gmail.com>
2013-09-06 19:10:35 -0700

    Work on p156

--- src/euler/p156.clj
+++ src/euler/p156.clj
@@ -2,10 +2,10 @@
 
 (defn f [n d]
   (loop [q 10
-         r 0]
+         r 0N]
     (let [left (quot n q)
           right (mod n q)
-          r (int (+ r 
+          r (bigint (+ r 
                     (* left q 0.1)
                     (if (= q 10)
                       (if (>= right d) 1 0)
@@ -14,7 +14,17 @@
         r
         (recur (* q 10) r)))))
 
-(f 10 1)
+(loop [n 1]
+  (when (< n 1E15)
+    (println n (f n 1) (- n (f n 1)))
+    (let [n2 (* 2 n)]
+      (println n2 (f n2 1) (- n2 (f n2 1))))
+    (recur (* n 10))))
+
+(for [x (range 199981 200020)
+      :let [a (f x 1)]
+      :when (= x a)]
+  (println x))
 
 
 
