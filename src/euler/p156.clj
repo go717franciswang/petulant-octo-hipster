76ceed6f0f1b52c1b2506728bab006dcec55180c
francis <go717franciswang@gmail.com>
2013-09-06 18:40:33 -0700

    Fix f(n,d)

--- src/euler/p156.clj
+++ src/euler/p156.clj
@@ -1,51 +1,20 @@
 (ns euler.p156)
 
-(defn diff-f [n1 n2 k]
-  " = f(n2,k) - f(n1,k)"
-  (let [digits2 (reverse (map #(read-string (str %)) (str n2)))
-        l (count digits2)
-        digits1 (reverse (map #(read-string (str %)) (str n1)))
-        digit-diff (reduce + (map
-                     (fn [d1 d2]
-                       (if (and (< d1 k) (<= k d2)) 1 0))
-                     (concat digits1 (repeat 0)) digits2))
-        carry-diff (loop [digits1 digits1
-                          digits2 digits2
-                          i 1
-                          r 0N]
-                     (if (empty? digits2)
-                       r
-                       (recur (rest digits1)
-                              (rest digits2)
-                              (* i 10)
-                              (+ r
-                                (* i 
-                                   (+
-                                     (let [d1 (or (first digits1) 0)
-                                           d2 (first digits2)]
-                                       (if (and (< d1 k) (<= k d2)) 1 0))
-                                     (- (if (empty? (rest digits2)) 0
-                                          (read-string (apply str (reverse (rest digits2)))))
-                                       (if (empty? (rest digits1)) 0 
-                                         (read-string (apply str (reverse (rest digits1))))))))))))]
-    (println digit-diff carry-diff)
-    (+ 0 carry-diff)))
-
-;(diff-f 0 12 1)
-
 (defn f [n d]
-  (let [digits (vec (reverse (map #(read-string (str %)) (str n))))
-        l (count digits)]
-    (for [i (range l)
-          :let [digit (digits i)
-                num-base (if (zero? i) (read-string (apply str (reverse (subvec digits 0 i))))
-                base (if (>= digit d) 1 0)
-                other-digits (* (Math/pow 10 i) 
-                                (if (< i (dec l))
-                                  (read-string (apply str (reverse (subvec digits (inc i))))) 0))]]
-      (+ base other-digits))))
+  (loop [q 10
+         r 0]
+    (let [left (quot n q)
+          right (mod n q)
+          r (int (+ r 
+                    (* left q 0.1)
+                    (if (= q 10)
+                      (if (>= right d) 1 0)
+                      (max (- (min (inc right) (* (inc d) q 0.1)) (* d q 0.1)) 0))))]
+      (if (zero? left)
+        r
+        (recur (* q 10) r)))))
 
-(f 12 1)
+(f 10 1)
 
 
 
