e967e14bd35b077dc0765b85b3ccebc6a9d6f9d9
Francis Wang <go717franciswang@hotmail.com>
2013-09-10 23:58:34 -0700

    Solve p206

--- src/euler/p206.clj
+++ src/euler/p206.clj
@@ -1,33 +1,21 @@
 (ns euler.p206
   (:require [clojure.math.combinatorics :as combo]))
 
-; 9_0 => end with 30
-; 1a2b3c4d5e6f7g8h900
+(def pattern "1.2.3.4.5.6.7.8.9.0$")
 
-(def digits (map str (range 10)))
-(def one-2-eight (map str (range 1 9)))
-
-(defn square? [n]
-  (let [a (Math/sqrt n)]
-    (== a (bigint a))))
-
-(loop [nums (apply combo/cartesian-product (repeat 8 digits))]
-  (let [n (first nums)
-        n1 (+ (* (read-string (apply str (interleave one-2-eight n))) 1000) 900)]
-    (println n1)
-    (if (square? n1)
-      (Math/sqrt n1)
-      (recur (rest nums)))))
-
-; (for [a digits
-;       b digits
-;       c digits
-;       d digits
-;       e digits
-;       f digits
-;       g digits
-;       h digits
-;       :let [n (read-string (str "1" a "2" b "3" c "4" d "5" e "6" f "7" g "8" h "900"))
-;             _ (println n)]
-;       :when (square? n)]
-;   (println n))
+(loop [l 1
+       candidates [0]]
+  (println l (count candidates))
+  (if (== l 10)
+    (let [patt (re-pattern pattern)]
+      (filter #(re-matches patt (str (* % %))) candidates))
+    (let [patt (re-pattern (str ".*" (subs pattern (- (count pattern) (+ 2 l)))))
+          m (reduce * (repeat l 10N))]
+      (recur 
+        (inc l)
+        (for [candidate candidates
+              digit (range 10)
+              :let [n (+ (* m digit) candidate)
+                    s (str (* n n))]
+              :when (re-matches patt s)]
+          n)))))
