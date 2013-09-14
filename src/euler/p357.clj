c147a3ecb93f6617485dd0e2891360bf35281ac5
francis <go717franciswang@gmail.com>
2013-09-13 17:54:50 -0700

    Solve p357

--- src/euler/p357.clj
+++ src/euler/p357.clj
@@ -11,42 +11,43 @@
 
 (defn all-divisors [factors]
   (let [l (quot (count factors) 2)]
-    (for [l0 (range 1 (+ 2 l))
+    (for [l0 (range 1 (inc l))
           f0 (combo/combinations factors l0)]
       (reduce * f0))))
 
 (defn valid? [n factors]
   (every?
     (fn [d]
-      (.isProbablePrime (biginteger (+ d (/ n d))) 15))
+      (.isProbablePrime (biginteger (+ d (/ n d))) 10))
     (all-divisors factors)))
 
+(time
 (reduce
   (fn [sum p]
     (let [d (- p 2)
           n (* 2 d)]
       (cond 
         (> n cap) sum
-        (not (.isProbablePrime (biginteger (inc (* 2 d))) 15)) sum
-        (.isProbablePrime (biginteger d) 15) (do (println n) (+ sum n))
+        (not (.isProbablePrime (biginteger (inc (* 2 d))) 10)) sum
+        (.isProbablePrime (biginteger d) 10) (+ sum n)
         :else (let [limit (Math/floor (Math/sqrt n))
-                    factors (loop [n0 n
+                    factors (loop [n0 d
                                    ps (rest primes)
                                    factors '(2)]
                               (let [p (first ps)]
-                                (if (or (> p limit) (== p 1))
-                                  factors
-                                  (let [mod0 (zero? (mod n0 p))]
-                                    (cond 
-                                      (and mod0 (== p (first factors))) []
-                                      mod0 (recur (/ n0 p) ps (conj factors p))
-                                      :else (recur n0 (rest ps) factors))))))]
+                                (cond
+                                  (== n0 1) factors
+                                  (> p limit) (conj factors n0)
+                                  (zero? (mod n0 p)) (if (== p (first factors)) 
+                                                       '()
+                                                       (recur (/ n0 p) ps (conj factors p)))
+                                  :else (recur n0 (rest ps) factors))))]
                 (if (empty? factors)
                   sum
                   (if (valid? n factors)
-                    (do (println n) (+ sum n))
+                    (+ sum n)
                     sum))))))
   1
-  (rest primes))
+  (rest primes)))
 
 
