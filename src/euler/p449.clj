b7735306546bd161dacaff77d2e1615a172a2546
Francis Wang <go717franciswang@hotmail.com>
2013-12-21 16:15:11 -0800

    Start p449

---
+++ src/euler/p449.clj
@@ -0,0 +1,9 @@
+(ns euler.p449)
+
+; general ellipsoid
+; x^2/a'^2 + y^2/b'^2 + z^2/c'^2 = 1 => a'^2 = b'^2 = a^2*b, c'^2 = a*b^2
+; => oblate ellipsoid (http://en.wikipedia.org/wiki/Oblate_spheroid)
+; => Surface area = ...
+; or calculate change in volume for adding that one uniform layer of chocolate?
+; need to verify against test cases
+
