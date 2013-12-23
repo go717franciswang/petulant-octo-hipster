a6ecef33c4a943546d59eb18e5de47e03db5a64f
francis <go717franciswang@gmail.com>
2013-12-23 14:17:20 -0800

    Solve p449

--- src/euler/p449.clj
+++ src/euler/p449.clj
@@ -1,54 +1,31 @@
 (ns euler.p449)
 
-; general ellipsoid
-; x^2/a'^2 + y^2/b'^2 + z^2/c'^2 = 1 => a'^2 = b'^2 = a^2*b, c'^2 = a*b^2
-; => oblate ellipsoid (http://en.wikipedia.org/wiki/Oblate_spheroid)
-; => Surface area = ...
-; or calculate change in volume for adding that one uniform layer of chocolate?
-; need to verify against test cases
-
 ; the shortest distance from the ellipse to the outer layer is formed by a projection
 ; that is perpendicular to the surface
-; A = pi*a*b, V = 4/3*pi*a^2*b => A * 4/3 * a = V
+; look at z-y plane, for any (z,y) on candy derive (z0,y0) on cholate 
+; and do numerical integration
 
-(def a 2)
+(def a 3)
 (def b 1)
-(def d 0.00001)
+(def d 0.000001)
 
 (def V
-  (* 4/3 (+ a 1) 4
-    (loop [x 0.0
-           x0 0.0
-           y0 (+ a d)
-           A 0.0]
-      (if (<= x a)
-        (let [y (Math/sqrt (* (- 1 (/ (* x x) a a)) b b))
-              q (Math/pow (/ b a) 2)
-              slope (/ -1 (* (Math/pow (- (* b b) (* q x x)) -0.5) -1 (* q x)))
-              dx (Math/sqrt (/ 1 (+ 1 (* slope slope))))
-              dy (if (zero? x) 1.0 (* slope dx))
-              x1 (+ x dx)
-              y1 (+ y dy)
-              A (+ A (* (+ y0 y1) (- x1 x0) 0.5))]
-          (recur (+ x d) x1 y1 A))
-        A))))
-
-#_(def V
   (* 2
-    (loop [x 0.0
-           x0 0.0
-           y0 (+ a d)
+    (loop [z 0.0
+           z0 0.0
+           y0 (+ a 1.0)
            V 0.0]
-      (if (<= x a)
-        (let [y (Math/sqrt (* (- 1 (/ (* x x) a a)) b b))
+      (if (<= z b)
+        (let [y (Math/sqrt (* (- 1 (/ (* z z) b b)) a a))
               q (Math/pow (/ b a) 2)
-              slope (/ -1 (* (Math/pow (- (* b b) (* q x x)) -0.5) -1 (* q x)))
-              dx (Math/sqrt (/ 1 (+ 1 (* slope slope))))
-              dy (if (zero? x) 1.0 (* slope dx))
-              x1 (+ x dx)
+              m (* -1 z a a (/ 1 b b) (Math/pow (* a a (- 1 (/ (* z z) b b))) -0.5))
+              slope (/ -1 m)
+              dz (Math/sqrt (/ 1 (+ 1 (* slope slope))))
+              dy (if (zero? z) 1.0 (* slope dz))
+              z1 (+ z dz)
               y1 (+ y dy)
-              V (+ V (* (+ (* Math/PI y0 y0) (* Math/PI y1 y1)) (- x1 x0) 0.5))]
-          (recur (+ x d) x1 y1 V))
+              V (+ V (* Math/PI (+ (* y0 y0) (* y1 y1)) 0.5 (- z1 z0)))]
+          (recur (+ z d) z1 y1 V))
         V))))
 
 (- V (* 4/3 Math/PI a a b))
