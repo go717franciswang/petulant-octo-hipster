edf2f0640dc8bbdd8d8fbba245477eda271b2baa
Francis Wang <go717franciswang@hotmail.com>
2013-12-22 16:39:19 -0800

    Approximate p449

--- src/euler/p449.clj
+++ src/euler/p449.clj
@@ -7,3 +7,48 @@
 ; or calculate change in volume for adding that one uniform layer of chocolate?
 ; need to verify against test cases
 
+; the shortest distance from the ellipse to the outer layer is formed by a projection
+; that is perpendicular to the surface
+; A = pi*a*b, V = 4/3*pi*a^2*b => A * 4/3 * a = V
+
+(def a 2)
+(def b 1)
+(def d 0.00001)
+
+(def V
+  (* 4/3 (+ a 1) 4
+    (loop [x 0.0
+           x0 0.0
+           y0 (+ a d)
+           A 0.0]
+      (if (<= x a)
+        (let [y (Math/sqrt (* (- 1 (/ (* x x) a a)) b b))
+              q (Math/pow (/ b a) 2)
+              slope (/ -1 (* (Math/pow (- (* b b) (* q x x)) -0.5) -1 (* q x)))
+              dx (Math/sqrt (/ 1 (+ 1 (* slope slope))))
+              dy (if (zero? x) 1.0 (* slope dx))
+              x1 (+ x dx)
+              y1 (+ y dy)
+              A (+ A (* (+ y0 y1) (- x1 x0) 0.5))]
+          (recur (+ x d) x1 y1 A))
+        A))))
+
+#_(def V
+  (* 2
+    (loop [x 0.0
+           x0 0.0
+           y0 (+ a d)
+           V 0.0]
+      (if (<= x a)
+        (let [y (Math/sqrt (* (- 1 (/ (* x x) a a)) b b))
+              q (Math/pow (/ b a) 2)
+              slope (/ -1 (* (Math/pow (- (* b b) (* q x x)) -0.5) -1 (* q x)))
+              dx (Math/sqrt (/ 1 (+ 1 (* slope slope))))
+              dy (if (zero? x) 1.0 (* slope dx))
+              x1 (+ x dx)
+              y1 (+ y dy)
+              V (+ V (* (+ (* Math/PI y0 y0) (* Math/PI y1 y1)) (- x1 x0) 0.5))]
+          (recur (+ x d) x1 y1 V))
+        V))))
+
+(- V (* 4/3 Math/PI a a b))
