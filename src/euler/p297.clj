db152cbdadaec0b8782cf050dffefce4eb57dd52
Francis Wang <go717franciswang@hotmail.com>
2014-01-14 00:18:00 -0800

    Solve p297

--- src/euler/p297.clj
+++ src/euler/p297.clj
@@ -1,7 +1,6 @@
 (ns euler.p2970)
 
-(def cap (bigint 1E17))
-(def cap (bigint 1E6))
+(def cap (dec (bigint 1E17)))
 
 (defn fib [a b] (cons a (lazy-seq (fib b (+ b a)))))
 
@@ -33,4 +32,43 @@
     (recur (inc i) (+ sum (z i)))))
 
 ; feed to oeis, and got http://oeis.org/A007895
-; M(i) = M(i-1) . inc(M(i-2))
+; observe pattern
+; S1 1 : 1
+; S2 2 : 1
+; S3 3 : 12
+; S4 5 : 122
+; S5 8 : 12223
+; S6 13: 12223233
+; S7 21: 1222323323334
+; S8 34: 122232332333423334344
+; ...
+
+(defn max-i [remainder C]
+  (loop [i 0]
+    (if (> (get C (inc i)) remainder)
+      i
+      (recur (inc i)))))
+
+(time
+  (loop [i 1
+         C [1N 1N]
+         S [1N 1N]
+         T 2N]
+    (let [new-T (+ T (+ (get C i) (get C (dec i))))]
+      (if (> new-T cap)
+        (let [S-sum (reduce + (take (inc i) S))]
+          (loop [remainder (- cap T)
+                 depth 0
+                 additional-sum 0N]
+            (let [i (max-i remainder C)
+                  c (get C i)]
+              (if (zero? remainder)
+                (+ S-sum additional-sum)
+                (recur (- remainder c) 
+                       (inc depth) 
+                       (+ additional-sum (get S i) (* c depth)))))))
+        (recur (inc i)
+               (conj C (+ (get C i) (get C (dec i))))
+               (conj S (+ (get S i) (get S (dec i)) (get C (dec i))))
+               (+ T (+ (get C i) (get C (dec i)))))))))
+
