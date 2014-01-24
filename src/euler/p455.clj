adfa04b0fb19cdaeedd382bc281735ee1a02d7b4
Francis Wang <go717franciswang@hotmail.com>
2014-01-24 00:46:19 -0800

    Find a new strategy

--- src/euler/p455.clj
+++ src/euler/p455.clj
@@ -1,12 +1,10 @@
 (ns euler.p455
   (:require [euler.helper :as h]))
 
-(loop [i 1
-       a #{}]
-  (let [e (h/pow-mod 4 i (bigint 1E9))]
-    (println i e)
-    (if (contains? a e)
-      (println "duplicate found")
-      (recur (inc i) (conj a e)))))
-
-
+; using the hint of f(4) = 411728896, we see that
+; 4^6 % 10 = 6
+; 4^96 % 100 = 96
+; 4^896 % 1000 = 896
+; ...
+; 4^11728896 % 1e8 = 11728896
+; hence we can devise a lazy strategy to search for the answer
