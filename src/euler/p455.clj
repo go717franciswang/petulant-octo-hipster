a184f9b587218a960e179f2d0b496d30d7626591
francis <go717franciswang@gmail.com>
2014-01-24 18:55:38 -0800

    Gain more insight

--- src/euler/p455.clj
+++ src/euler/p455.clj
@@ -8,3 +8,44 @@
 ; ...
 ; 4^11728896 % 1e8 = 11728896
 ; hence we can devise a lazy strategy to search for the answer
+; strategy:
+; base case search for single digits d exponential such that x^d % 10 = d
+; inductive step, for i-digit numbers that satisfy x^d % 10^i = d,
+;                   find (i+1)-digit numbers that satisfy x^d % 10^(i+1) = d
+
+(def possible-base-cases [6 7 5 3 9])
+
+(defn base-case [n]
+  (if (= n 1)
+    1
+    (first (filter #(= % (h/pow-mod n % 10)) possible-base-cases))))
+
+(defn get-largest [answers]
+  (reduce max answers))
+
+(defn f [n]
+  (let [base-case (base-case n)]
+    (if (nil? base-case)
+      0
+      (loop [digits 2
+             answers [base-case]]
+        (if (= digits 10)
+          (get-largest answers)
+          (let [m1 (int (Math/pow 10 (dec digits)))
+                m2 (* m1 10)
+                new-answers (for [i (range 1 10)
+                                  :let [a (* m1 i)]
+                                  answer answers
+                                  :let [new-num (+ answer a)
+                                        _ (println n new-num m2)]
+                                  :when (= new-num (h/pow-mod n new-num m2))]
+                              new-num)]
+            (if (empty? new-answers)
+              (get-largest answers)
+              (recur (inc digits) new-answers))))))))
+
+(f 157)
+
+(base-case 157)
+(h/pow-mod 157 157 1000)
+; looks like for longer digits we need a different test for base case
